9ccf4b456ebc7723c10cc81b850d7c8ea0193ad6
🍙 𝑻𝒐𝒌𝒊𝒚𝒖 🎋 <134459455+TokynBlast@users.noreply.github.com>
2025-10-28 12:45:02 -0700

    added organizing

--- toml.cpp
+++ toml.cpp
@@ -5,4 +5,6 @@
 // in between bytes 0x527572694d696e546f6b
 /*
 max_thread / int / max number of threads a with {} and {} statement can have
+organize / bool / whether to sort everything by
+                   scope into variables -> functions -> everything else 
 */
