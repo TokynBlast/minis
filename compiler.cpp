0c660cb192e4c37ace48a6671574f684c7425bd2
🍙 𝑻𝒐𝒌𝒊𝒚𝒖 🎋 <134459455+TokynBlast@users.noreply.github.com>
2025-10-29 13:24:32 -0700

    Added include of io.hpp

--- compiler.cpp
+++ compiler.cpp
@@ -1,12 +1,4 @@
-inline static void write_u8 (FILE*f, uint8_t  v){ fwrite(&v,1,1,f); }
-inline static void write_u64(FILE*f, uint64_t v){ fwrite(&v,8,1,f); }
-inline static void write_s64(FILE*f, int64_t  v){ fwrite(&v,8,1,f); }
-inline static void write_f64(FILE*f, double   v){ fwrite(&v,8,1,f); }
-inline static void write_str(FILE*f, const std::string&s){ uint64_t n=s.size(); write_u64(f,n); if(n) fwrite(s.data(),1,n,f); }
-
-// inline static uint8_t  read_u8 (FILE*f){ uint8_t v;  fread(&v,1,1,f); return v; }
-// inline static uint64_t read_u64(FILE*f){ uint64_t v; fread(&v,8,1,f); return v; }
-// inline static std::string read_str(FILE*f){ uint64_t n=read_u64(f); std::string s; s.resize(n); if(n) fread(&s[0],1,n,f); return s; }
+#inclide "../include/io.hpp"
 
 struct Compiler {
   Pos P;
@@ -14,6 +6,10 @@ struct Compiler {
   std::vector<FnInfo> fns;
   std::unordered_map<std::string,size_t> fnIndex;
   bool inWith;
+
+  std::unique_ptr<SProgram> parseProgram() }{
+    suto prog = std::make_unique<SProgram>();
+  }
 
   // minis header fields
   uint64_t table_offset_pos=0, fn_count_pos=0, entry_main_pos=0;
