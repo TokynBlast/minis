81af36525085bf193e2d5cfffa99ad357f446d59
🍙 𝑻𝒐𝒌𝒊𝒚𝒖 🎋 <134459455+TokynBlast@users.noreply.github.com>
2025-10-29 12:30:20 -0700

    Changed Type to DType (data type)

--- include/types.hpp
+++ include/types.hpp
@@ -2,22 +2,23 @@
 #include <string>
 
 namespace minis {
-enum class Type : unsigned char {
+enum class DType : unsigned char {
   Int=0, Float=1, Bool=2, Str=3, List=4, Null=5,
   ERR=250, UNKNOWN=251
 };
 
-inline const char* TypeName(Type t){
+inline const char* TypeName(DType t){
   switch(t){
-    case Type::Int: return "int";
-    case Type::Float: return "float";
-    case Type::Bool: return "bool";
-    case Type::Str: return "str";
-    case Type::List: return "list";
-    case Type::Null: return "null";
-    case Type::ERR: return "<ERR>";
-    case Type::UNKNOWN: return "<UNKNOWN>";
+    case DType::Int: return "int";
+    case DType::Float: return "float";
+    case DType::Bool: return "bool";
+    case DType::Str: return "str";
+    case DType::List: return "list";
+    case DType::Null: return "null";
+
+    case DType::ERR: return "<ERR>";
+    case DType::UNKNOWN: return "<UNKNOWN>";
   }
   return "?";
 }
-} // namespace minis
+}
