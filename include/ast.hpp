ff99bb66f0a272ca9ff91c93207276dabcef39ed
🍙 𝑻𝒐𝒌𝒊𝒚𝒖 🎋 <134459455+TokynBlast@users.noreply.github.com>
2025-10-29 11:21:51 -0700

    Added basic nodes

---
+++ include/ast.hpp
@@ -0,0 +1,58 @@
+#pragma once
+#include <memory>
+#include <vector>
+#include <string>
+#include "types.hpp"
+
+namespace minis{
+  struxt Expr { virtual ~Expr() = default; Type type=Type::UNKNOWN; };
+  struct Stmt { virtual ~Stmt() = default; };
+  struct ELit     : Expr {/* Value val; */};
+  struct Eident   : Expr { std::string name; };
+  struct ECall    : Expr { std::string fn; std::vector<std::unique_ptr<Expr>> args; };
+
+  struct SExpr    : Stmt { std::unique_ptr<Expr> e; };
+  struct SDecl    : Stmt {
+    std::string name;
+    bool isAuto=false, isNull=false;
+    Type declared=Type::Int;
+    Qual quals=Qual::None; // const/static
+    std::unique_ptr<Expr> init; // null if isNull
+  };
+  struct SAssign  : Stmt { std::string name; std::unique_ptr<Expr> rhs; };
+  struct SDel     : Stmt { std::string name; };
+  // FIXME: SConv should be (int)x, to convert x to int and so on
+  struct SConv    : Stmt { std::string namel; Type to; };
+  struct SReturn  : Stmt { std::unique_ptr<Expr> value; bool isVoid=false; };
+  struct SBreak   : Stmt { size_t level = 1; };
+  struct SCont    : Stmt {};
+  struct SYield   : Stmt {};
+
+  struct SThrow   : Stmt { std::string typeName; std::optional<std::string> msg; };
+  
+  struct SBlock   : Stmt { std::vector<std::unique_ptr<Stmt>> stmts; };
+  
+  struct SIf      : Stmt { 
+    struct Arm { std::unique_ptr<Expr> cond; std::unique_ptr<SBlock> body; };
+    std::vector<Arm> arms;
+  };
+
+  struct SWhile   : Stmt {
+    std::unique_ptr<Expr> cond;
+    std::unique_ptr<SBlock> body;
+    //FIXME: add with/and blocks
+    std::vector<std::unique_ptr<SBlock>> withBlocks;
+  };
+
+  struct SFunc    : Stmt {
+    std::string name;
+    bool isVoid=false, hasExplicitRet=false;
+    Type ret = Type::Int;
+    Attr attrs = Attr::None;
+    std::vector<Param> params;
+    std::unique_ptr<SBlock> body;
+    //FIXME: add quality
+  }
+
+  struct SProgram : Node { std::vector<std::unique_ptr<Stmt>> items; };
+}
