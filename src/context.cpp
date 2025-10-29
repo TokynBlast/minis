219c1026c97bd2d9877fd249f975f727d8658810
🍙 𝑻𝒐𝒌𝒊𝒚𝒖 🎋 <134459455+TokynBlast@users.noreply.github.com>
2025-10-29 08:45:44 -0700

    Removed namespace comment

--- src/context.cpp
+++ src/context.cpp
@@ -1,9 +1,9 @@
 #include "../include/context.hpp"
 namespace minis {
-static Context* G = nullptr;
-Context& ctx(){ static Context C; if(!G) G=&C; return *G; }
+  static Context* G = nullptr;
+  Context& ctx(){ static Context C; if(!G) G=&C; return *G; }
 
-void diag(Diagnostic::Kind k, size_t beg, size_t end, std::string msg){
-  ctx().diags.push_back({k, Span{map_pos(beg), map_pos(end)}, std::move(msg)});
+  void diag(Diagnostic::Kind k, size_t beg, size_t end, std::string msg){
+    ctx().diags.push_back({k, Span{map_pos(beg), map_pos(end)}, std::move(msg)});
+  }
 }
-} // namespace minis
