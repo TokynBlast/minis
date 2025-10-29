5261425d3d4b107e748d9c71057f7dea23b898ee
🍙 𝑻𝒐𝒌𝒊𝒚𝒖 🎋 <134459455+TokynBlast@users.noreply.github.com>
2025-10-29 08:23:38 -0700

    Added the lexer

---
+++ include/lexer.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include <vector>
+#include <string>
+#include "../include/token.hpp"
+
+namespace minis{
+    struct Lexer{
+        const std::string* src=nullptr; size_t i=0, n=0;
+        std::vector<Token> out;
+        explicit     Lexer(const std::strinf& s){ src=&s; n=s.size(); run(); }
+        static bool  isIdStart(char c);
+        static bool  isIdCont(char c);
+        static TKind keyword(const std::string& t)
+    };
+}
