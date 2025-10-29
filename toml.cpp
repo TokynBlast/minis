3a0616765a3437bbdfeadc498f5ac2001b05c9e6
🍙 𝑻𝒐𝒌𝒊𝒚𝒖 🎋 <134459455+TokynBlast@users.noreply.github.com>
2025-10-29 14:13:04 -0700

    Added new paramaters

--- toml.cpp
+++ toml.cpp
@@ -4,7 +4,9 @@
 // all data here stored in binary
 // in between bytes 0x527572694d696e546f6b
 /*
-max_thread / int / max number of threads a with {} and {} statement can have
 organize / bool / whether to sort everything by
-                   scope into variables -> functions -> everything else 
+                   scope into variables -> functions -> everything else
+dev / string / name of dev/publisher; no offical format yet
+time_include / bool / whether to include an epoch of time code compiled
+code_version / string or int / version of code
 */
