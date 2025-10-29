809fcfeea60f8bc4a25a694293f5143c04384b6b
🍙 𝑻𝒐𝒌𝒊𝒚𝒖 🎋 <134459455+TokynBlast@users.noreply.github.com>
2025-10-29 12:31:46 -0700

    Added spacing [cosmetic]

--- include/types.hpp
+++ include/types.hpp
@@ -2,23 +2,23 @@
 #include <string>
 
 namespace minis {
-enum class DType : unsigned char {
-  Int=0, Float=1, Bool=2, Str=3, List=4, Null=5,
-  ERR=250, UNKNOWN=251
-};
+  enum class DType : unsigned char {
+    Int=0, Float=1, Bool=2, Str=3, List=4, Null=5,
+    ERR=250, UNKNOWN=251
+  };
 
-inline const char* TypeName(DType t){
-  switch(t){
-    case DType::Int: return "int";
-    case DType::Float: return "float";
-    case DType::Bool: return "bool";
-    case DType::Str: return "str";
-    case DType::List: return "list";
-    case DType::Null: return "null";
+  inline const char* TypeName(DType t){
+    switch(t){
+      case DType::Int: return "int";
+      case DType::Float: return "float";
+      case DType::Bool: return "bool";
+      case DType::Str: return "str";
+      case DType::List: return "list";
+      case DType::Null: return "null";
 
-    case DType::ERR: return "<ERR>";
-    case DType::UNKNOWN: return "<UNKNOWN>";
+      case DType::ERR: return "<ERR>";
+      case DType::UNKNOWN: return "<UNKNOWN>";
+    }
+    return "?";
   }
-  return "?";
-}
 }
