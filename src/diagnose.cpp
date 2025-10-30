15162838198bd924b5148aa6be43ca4c21d9c23c
🍙 𝑻𝒐𝒌𝒊𝒚𝒖 🎋 <134459455+TokynBlast@users.noreply.github.com>
2025-10-30 14:09:26 -0700

    Added include diagnose.hpp

--- src/diagnose.cpp
+++ src/diagnose.cpp
@@ -1,17 +1,20 @@
 #include <vector>
 #include <string>
 #include "../include/context.hpp"
+#include "../include/diagnose.hpp"
 
-struct Diagnostic {
-  enum Kind { Error, Warning, Note } k;
-  Span span; std::string msg;
-};
-static std::vector<Diagnostic> g_diags;
+namespace minis {
+  struct Diagnostic {
+    enum Kind { Error, Warning, Note } k;
+    Span span; std::string msg;
+  };
+  static std::vector<Diagnostic> g_diags;
 
-inline void DIAG(Diagnostic::Kind k, size_t beg, size_t end, std::string m){
-  g_diags.emplace_back(k, Span{minis::map_pos(beg), minsi::map_pos(end)}, std::move(m));
-}
-inline bool hasErrors(){
-  for (auto& d : g_diags) if (d.k==Diagnostic::Error) return true;
-  return false;
+  inline void DIAG(Diagnostic::Kind k, size_t beg, size_t end, std::string m){
+    g_diags.emplace_back(k, Span{minis::map_pos(beg), minsi::map_pos(end)}, std::move(m));
+  }
+  inline bool hasErrors(){
+    for (auto& d : g_diags) if (d.k==Diagnostic::Error) return true;
+    return false;
+  }
 }
