4d6c3b8d0d73abf7c32c8d2622ae73915a94dece
🍙 𝑻𝒐𝒌𝒊𝒚𝒖 🎋 <134459455+TokynBlast@users.noreply.github.com>
2025-10-30 14:13:19 -0700

    Changed Kind to EKind

--- include/diagnose.hpp
+++ include/diagnose.hpp
@@ -5,12 +5,12 @@
 
 namespace minis {
   struct Diagnostic {
-    enum Kind { Error, Warning, Note } k;
+    enum EKind { Error, Warning, Note } k;
     Span span; std::string msg;
   };
 
   extern std::vector<Diagnostic> g_diags;
 
-  void DIAG(Diagnostic::Kind k, size_t beg, size_t end, std::string m);
+  void DIAG(Diagnostic::EKind k, size_t beg, size_t end, std::string m);
   bool hasErrors();
 }
--- src/diagnose.cpp
+++ src/diagnose.cpp
@@ -5,13 +5,13 @@
 
 namespace minis {
   struct Diagnostic {
-    enum Kind { Error, Warning, Note } k;
+    enum EKind { Error, Warning, Note } k;
     Span span; std::string msg;
   };
   static std::vector<Diagnostic> g_diags;
 
-  inline void DIAG(Diagnostic::Kind k, size_t beg, size_t end, std::string m){
-    g_diags.emplace_back(k, Span{minis::map_pos(beg), minsi::map_pos(end)}, std::move(m));
+  inline void DIAG(Diagnostic::EKind k, size_t beg, size_t end, std::string m){
+    g_diags.emplace_back(k, Span{minis::map_pos(beg), minis::map_pos(end)}, std::move(m));
   }
   inline bool hasErrors(){
     for (auto& d : g_diags) if (d.k==Diagnostic::Error) return true;
