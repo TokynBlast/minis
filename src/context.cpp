fbe524684ede1abeee810afcc0f2b68d42175511
🍙 𝑻𝒐𝒌𝒊𝒚𝒖 🎋 <134459455+TokynBlast@users.noreply.github.com>
2025-10-30 11:48:43 -0700

    Fixed Kind name

--- src/context.cpp
+++ src/context.cpp
@@ -3,7 +3,7 @@ namespace minis {
   static Context* G = nullptr;
   Context& ctx(){ static Context C; if(!G) G=&C; return *G; }
 
-  void diag(Diagnostic::Kind k, size_t beg, size_t end, std::string msg){
+  void diag(Diagnostic::EKind k, size_t beg, size_t end, std::string msg){
     ctx().diags.push_back({k, Span{map_pos(beg), map_pos(end)}, std::move(msg)});
   }
 }
