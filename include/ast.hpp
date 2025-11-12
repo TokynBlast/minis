4d98a59706aba7d492ae75f99d71075dbcd1ce74
🍙 𝑹𝒖𝒓𝒊 🎋 <134459455+TokynBlast@users.noreply.github.com>
2025-11-12 21:36:47 +0000

    Changed std::string to CString

--- include/ast.hpp
+++ include/ast.hpp
@@ -1,59 +1,73 @@
 #pragma once
 #include <memory>
 #include <vector>
-#include <string>
 #include <optional>
 #include "types.hpp"
+#include "sso.hpp"
 
 namespace lang{
-  struct Param { std::string name; Type type = Type::Int; };
+  struct Param { CString name; Type type = Type::Int; };
   struct Expr { virtual ~Expr() = default; Type type=Type::UNKNOWN; };
-  struct Stmt { virtual ~Stmt() = default; };
-  struct ident   : Expr { std::string name; };
-  struct Call    : Expr { std::string fn; std::vector<std::unique_ptr<Expr>> args; };
+  struct Stmt { virtual ~Stmt() = default; size_t s = 0; }; // size :)
+  struct ident   : Expr { CString name; };
+  struct Call    : Expr { CString fn; std::vector<std::unique_ptr<Expr>> args; };
 
-  struct Expr    : Stmt { std::unique_ptr<Expr> e; };
   struct Decl    : Stmt {
-    std::string name;
+    CString name;
     bool isAuto=false, isNull=false;
     Type declared=Type::Int;
     bool isConst=false, isStatic=false; // const/static
     std::unique_ptr<Expr> init; // null if isNull
   };
-  struct SAssign  : Stmt { std::string name; std::unique_ptr<Expr> rhs; };
-  struct SDel     : Stmt { std::string name; };
+  struct SAssign  : Stmt { CString name; std::unique_ptr<Expr> rhs; };
+  struct SDel     : Stmt { CString name; };
   // FIXME: SConv should be (int)x, to convert x to int and so on
-  struct Conv    : Stmt { std::string name; Type to; };
-  struct Return  : Stmt { std::unique_ptr<Expr> value; bool isVoid=false; };
-  struct Break   : Stmt { size_t level = 1; };
-  struct Cont    : Stmt {};
-  struct Yield   : Stmt {};
+  struct Conv    : Stmt { CString name; Type to; size_t s = 4; };
+  struct Return  : Stmt { std::unique_ptr<Expr> value; bool isVoid=false; size_t s = 6; };
+  struct Break   : Stmt { size_t level = 1; size_t s = 5; };
+  struct Cont    : Stmt { size_t s = 8; };
+  struct Yield   : Stmt { size_t s = 5; };
 
-  struct WS       : Stmt { size_t size = 0; };
+  struct WS      : Stmt { size_t s = 0; int NL=0; };
 
-  struct Throw   : Stmt { std::string typeName; std::optional<std::string> msg; };
-  
-  struct Block   : Stmt { std::vector<std::unique_ptr<Stmt>> stmts; };
-  
-  struct If      : Stmt { 
+  struct Throw   : Stmt { CString typeName; std::optional<CString> msg; size_t s = 5; };
+
+  //FIXME: Not sure how or if this needs or gets a size?
+  struct Block   : Stmt { std::vector<std::unique_ptr<Stmt>> stmts; size_t s = 1; };
+
+  struct If      : Stmt {
     struct Arm { std::unique_ptr<Expr> cond; std::unique_ptr<Block> body; };
     std::vector<Arm> arms;
+    size_t s = 2;
+  };
+  struct ElseIf  : Stmt {
+    struct Arm { std::unique_ptr<Expr> cond; std::unique_ptr<Block> body; };
+    std::vector<Arm> arms;
+    size_t s = 4;
+  };
+  struct Else    : Stmt {
+    struct Arm { std::unique_ptr<Expr> cond; std::unique_ptr<Block> body; };
+    std::vector<Arm> arms;
+    size_t s = 4;
   };
 
   struct While   : Stmt {
     std::unique_ptr<Expr> cond;
     std::unique_ptr<Block> body;
-    //FIXME: add with/and blocks
+    //FIXME: with and shouldn't be in while anymore
     std::vector<std::unique_ptr<Block>> withBlocks;
+    size_t s = 5;
   };
 
+  // exit, try, except, finally, lambda, import
   struct Func    : Stmt {
-    std::string name;
+    CString name;
     bool isVoid=false, hasExplicitRet=false;
     Type ret = Type::Int;
     std::vector<Param> params;
     std::unique_ptr<Block> body;
     //FIXME: add quality
+    size_t s = 4;
   };
 
   struct Program { std::vector<std::unique_ptr<Stmt>> items; };
