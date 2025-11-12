9caa64fe23fe32014d3be3ad651ccda94d5ad055
🍙 𝑹𝒖𝒓𝒊 🎋 <134459455+TokynBlast@users.noreply.github.com>
2025-11-12 22:01:53 +0000

    Fixed accepted type

--- include/compiler.hpp
+++ include/compiler.hpp
@@ -1,12 +1,15 @@
 #pragma once
 #include "sso.hpp"
+#include "token.hpp"
+#include <vector>
 
 namespace lang {
   struct Source;
 
   class Compiler {
   public:
-    explicit Compiler(const CString& src);
-    void compileToFile(const CString* out);
+    explicit Compiler(const std::vector<Token>& tokens);
+
+    void compileToFile(const CString& out);
   };
 }
