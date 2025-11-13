0e665119bfc6f1dc125c3846617c916616a556f1
🍙 𝑹𝒖𝒓𝒊 🎋 <134459455+TokynBlast@users.noreply.github.com>
2025-11-13 18:02:22 +0000

    Added IsId[Start/Cont] to lexer.hpp

--- include/lexer.hpp
+++ include/lexer.hpp
@@ -4,18 +4,19 @@
 #include "sso.hpp"
 
 namespace lang {
+  bool IsIdStart(char c);
+  bool IsIdCont(char c);
 
-class Lexer {
-private:
-    const CString* src = nullptr;
-    size_t i = 0, n = 0;
-    std::vector<Token> out;
+  class Lexer {
+  private:
+      const CString* src = nullptr;
+      size_t i = 0, n = 0;
+      std::vector<Token> out;
 
-public:
-    explicit Lexer(const CString& s);
-    void run();
-    const std::vector<Token>& tokens() const { return out; }
-    static Tok keyword(const CString& t);
-};
-
+  public:
+      explicit Lexer(const CString& s);
+      void run();
+      const std::vector<Token>& tokens() const { return out; }
+      static Tok keyword(const CString& t);
+  };
 }
