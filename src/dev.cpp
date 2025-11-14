811bc68e76299751385e813725821904ae0746f0
🍙 𝑹𝒖𝒓𝒊 🎋 <134459455+TokynBlast@users.noreply.github.com>
2025-11-13 23:19:29 -0800

    Added dev function start

--- src/dev.cpp
+++ src/dev.cpp
@@ -1,3 +1,20 @@
 // This is for the development of this library
 // This will become boot strapped (it means made in minis)
 
+#include <vector>
+#include <unordered_map>
+#include <function>
+#include "../include/io.hpp"
+#include "../include/token.hpp"
+#include "../include/type.hpp"
+
+using DevFn = std::function<Value(std::vector<Value>&)>;
+
+// Registry of dev functions
+inline static std::unordered_map<CString, BuiltinFn> builtins = {
+  {"dev.emitStr", [](std::vector<Value>& args) {
+    for (const auto& arg : args) std::cout << arg.AsStr() << " ";
+    emit
+    return Value::N();
+  }},
+}
