78c2ebf92ee518e889f2df0400f0f9be2057d7f2
🍙 𝑹𝒖𝒓𝒊 🎋 <134459455+TokynBlast@users.noreply.github.com>
2025-11-17 22:16:58 +0000

    Added hex code comments

--- include/bytecode.hpp
+++ include/bytecode.hpp
@@ -2,42 +2,42 @@
 #include <cstdint>
 enum Op : std::uint16_t {
   IMPORTED_FUNC  = 0x01,
-  IMPORTED_LOAD,
-  IMPORTED_STORE,
-  NOP,
-  PUSH_I,
-  PUSH_F,
-  PUSH_B,
-  PUSH_S,
-  PUSH_C,
-  PUSH_N,
-  MAKE_LIST,
-  GET,
-  SET,
-  DECL,
-  POP,
-  ADD,
-  SUB,
-  MUL,
-  DIV,
-  NEG,
-  EQ,
-  NE,
-  LT,
-  LE,
-  AND,
-  OR,
-  JMP,
-  JF,
-  CALL,
-  RET,
-  RET_VOID,
-  HALT,
-  UNSET,
-  SLICE,
-  INDEX,
-  SET_INDEX,
-  TAIL,
-  YIELD,
-  NOT
+  IMPORTED_LOAD,  // 02
+  IMPORTED_STORE, // 03
+  NOP,            // 04
+  PUSH_I,         // 05
+  PUSH_F,         // 06
+  PUSH_B,         // 07
+  PUSH_S,         // 08
+  PUSH_C,         // 09
+  PUSH_N,         // 0A
+  MAKE_LIST,      // 0B
+  GET,            // 0C
+  SET,            // 0D
+  DECL,           // 0E
+  POP,            // 0F
+  ADD,            // 10
+  SUB,            // 11
+  MUL,            // 12
+  DIV,            // 13
+  NEG,            // 14
+  EQ,             // 15
+  NE,             // 16
+  LT,             // 17
+  LE,             // 18
+  AND,            // 19
+  OR,             // 1A
+  JMP,            // 1B
+  JF,             // 1C
+  CALL,           // 1D
+  RET,            // 1E
+  RET_VOID,       // 1F
+  HALT,           // 20
+  UNSET,          // 21
+  SLICE,          // 22
+  INDEX,          // 23
+  SET_INDEX,      // 24
+  TAIL,           // 25
+  YIELD,          // 26
+  NOT             // 27
 };
