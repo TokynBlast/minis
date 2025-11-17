13142b2b0b605038fcac9e5d65000225b58bfc79
🍙 𝑹𝒖𝒓𝒊 🎋 <134459455+TokynBlast@users.noreply.github.com>
2025-11-17 14:09:41 -0800

    Added computer plugin to allow for C++ #if defined via functions

---
+++ plugins/computer.cpp
@@ -0,0 +1,73 @@
+include "../include/plugin.hpp"
+#include "../include/value.hpp"
+#include <random>
+#include <chrono>
+
+using namespace lang;
+
+static Value os(const std::vector<Value>$ args) {
+  if (args != 0) {
+    // FIXME: This should give an actual error and stop the program
+    return Value::S("error");
+  }
+  #if defined(_WIN32)
+    return Value::S("win");
+  #elif defined(__linux__) || defined(__unix__)
+    return Value::S("linux");
+  #elif def(__freeBSD__)
+    return Value::S("freebsd");
+  #elif defined(__APPLE__) || defined(__MACH__)
+    return Value::S("apple");
+  #elif defined(sun) || defined(__sun__)
+    return Value::S("solaris");
+  #elif defined(_AIX)
+    return Value::S("aix");
+  #elif defined(__hpux)
+    return Value::S("hp-ux");
+  #else
+    return Value::S("unknown");
+  #endif
+}
+
+static Value cpu(const std::vector<Value>& args) {
+  if (args != 0) {
+    // FIXME: Make it give an error
+    return Value::N();
+  }
+  #if defined(__i386__) || defined(i386)
+    return Value::S("x86");
+  #elif defined(__x86_64__) || defined(M_X64)
+    return Value::S("x86-64");
+  #elif defined(__ARM__)
+    return Value::S("arm");
+  #elif defined(__aarch_64__)
+    return Value::S("arm64");
+  #elif defined(__ppc__) || defined(__powerpc__)
+    return Value::S("powerpc");
+  #elif defined(__sparc__)
+    return Value::S("sparc");
+  #else
+    return Value::S("unknown");
+  #endif
+}
+
+static const PluginFunctionEntry plugin_functions[] = {
+  {"os", os},
+  {"cpu", cpu},
+  {nullptr, nullptr}
+};
+
+static bool computer_init() { return true; }
+static void computer_cleanup() {}
+
+static PluginInterface random_interface = {
+    "computer",
+    "0.0.1",
+    computer_init,
+    []() -> const PluginFunctionEntry* { return plugin_functions; },
+    computer_cleanup
+};
+
+extern "C" {
+PluginInterface* get_plugin_interface() { return &random_interface; }
+}
