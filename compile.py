925ae5e86ffb78d679ffa5f8fff4eacb182189d7
🍙 𝑹𝒖𝒓𝒊 🎋 <134459455+TokynBlast@users.noreply.github.com>
2025-12-08 14:02:22 -0800

    Updated opcode values
    Removed header comments and fixed syntax for constants.

--- compile.py
+++ compile.py
@@ -1,9 +1,4 @@
 #!/usr/bin/env python3
-"""
-Procedural Minis Compiler - Direct, top-down, minimal class usage
-- Supports: postfix ++, += operator, whitespace skipping, keyword matching
-- Emits AVOCADO1 bytecode
-"""
 
 import sys
 import struct
@@ -23,50 +18,50 @@ OPCODES = {
   'PUSH_INT': 0x04,
   'PUSH_FLOAT': 0x05,
   'PUSH_BOOL': 0x06,
-  'PUSH_STRING = 0x07,
-  'PUSH_NULL = 0x09,
-  'PUSH_LIST = 0x010,
+  'PUSH_STRING': 0x07,
+  'PUSH_NULL': 0x09,
+  'PUSH_LIST': 0x010,
 
   # variable stuff :)
-  VARIABLE_REGISTER = 0x0C,
+  'VARIABLE_REGISTER': 0x0C,
   'GET': 0x011,
   'SET': 0x012,
   'DECLARE': 0x013,
-  'UNSET': 0x15,
+  'UNSET': 0x14,
 
   // math opertions
   'MATH_REGISTER': 0x0D,
-    ADD = 0x016,
-    SUBTRACT = 0x017,
-    MULTIPLY = 0x018,
-    DIVIDE = 0x019,
-    NEGATE = 0x020,
+  'ADD': 0x015,
+  'SUBTRACT': 0x016,
+  'MULTIPLY': 0x017,
+  'DIVIDE': 0x018,
+  'NEGATE': 0x019,
 
-  LOGIC_REGISTER = 0x0E,
-    EQUAL = 0x020,
-    NOT_EQUAL = 0x21,
-    LESS_THAN_OR_EQUAL = 0x022,
-    LESS_THAN = 0x023,
-    AND = 0x024,
-    OR = 0x025,
-    JUMP = 0x026,
-    JUMP_IF = 0x027,
-    NOT_EQUAL = 0x28,
+  'LOGIC_REGISTER = 0x0E,
+  'EQUAL': 0x020,
+  'NOT_EQUAL': 0x21,
+  'LESS_THAN_OR_EQUAL':  0x022,
+  'LESS_THAN': 0x023,
+  'AND': 0x024,
+  'OR': 0x025,
+  'JUMP': 0x026,
+  'JUMP_IF': 0x027,
+  'NOT_EQUAL': 0x28,
 
 
-  FUNCTION_REGISTER = 0x0F,
-    CALL = 0x029,
-    TAIL = 0x030,
-    RETURN = 0x031,
-    RETURN_VOID = 0x032,
+  'FUNCTION_REGISTER': 0x0F,
+  'CALL': 0x029,
+  'TAIL': 0x030,
+  'RETURN': 0x031,
+  'RETURN_VOID': 0x032,
 
-  GENERAL_REGISTER = 0x01A
-    HALT = 0x033,
-    SLICE = 0x034,
-    SET_INDEX = 0x035,
-    NO_OP = 0x036,
-    POP = 0x037,
-    INDEX = 0x038,
+  'GENERAL_REGISTER': 0x01A
+  'HALT': 0x033,
+  'SLICE': 0x034,
+  'SET_INDEX': 0x035,
+  'NO_OP': 0x036,
+  'POP': 0x037,
+  'INDEX': 0x038
 }
 
 # --- Bytecode buffer and helpers ---
