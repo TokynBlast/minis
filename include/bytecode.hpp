6cc03d65a292fbd1981aec5c87f4e99793a913aa
🍙 𝑹𝒖𝒓𝒊 🎋 <134459455+TokynBlast@users.noreply.github.com>
2025-12-08 11:56:53 -0800

    Updated opcodes to have their own register

--- include/bytecode.hpp
+++ include/bytecode.hpp
@@ -3,53 +3,58 @@
 enum Op : std::uint16_t {
   // importing
   IMPORT_REGISTER = 0x0A,
-  IMPORT_FUNC  = 0x01,
-  IMPORT_LOAD = 0x02,
-  IMPORT_STORE = 0x03,
+    IMPORT_FUNC  = 0x01,
+    IMPORT_LOAD = 0x02,
+    IMPORT_STORE = 0x03,
 
   // making variables
   PUSH_REGISTER = 0x0B,
-  PUSH_INT = 0x04,
-  PUSH_FLOAT = 0x05,
-  PUSH_BOOL = 0x06,
-  PUSH_STRING = 0x07,
-  PUSH_C = 0x08,
-  PUSH_NULL = 0x09,
-  MAKE_LIST = 0x010,
+    PUSH_INT = 0x04,
+    PUSH_FLOAT = 0x05,
+    PUSH_BOOL = 0x06,
+    PUSH_STRING = 0x07,
+    PUSH_C = 0x08,
+    PUSH_NULL = 0x09,
+    PUSH_LIST = 0x010,
 
   // work with variables :3
-  VARIABLE = 0x0C,
-  GET = 0x,
-  SET = 0x4D,
-  DECL = ,
-  POP,            // 0F
+  VARIABLE_REGISTER = 0x0C,
+    GET = 0x011,
+    SET = 0x012,
+    DECLARE = 0x013,
+    UNSET = 0x15,
 
   // math opertions
-  ADD,            // 10
-  SUB,            // 11
-  MUL,            // 12
-  DIV,            // 13
-  NEG,            // 14
+  MATH_REGISTER = 0x0D
+    ADD = 0x016,
+    SUBTRACT = 0x017,
+    MULTIPLY = 0x018,
+    DIVIDE = 0x019,
+    NEGATE = 0x020,
 
-  EQ,             // 15
-  NE,             // 16
-  LT,             // 17
-  LE,             // 18
+  LOGIC_REGISTER = 0x0E,
+    EQUAL = 0x020,
+    NOT_EQUAL = 0x21,
+    LESS_THAN_OR_EQUAL = 0x022,
+    LESS_THAN = 0x023,
+    AND = 0x024,
+    OR = 0x025,
+    JUMP = 0x026,
+    JUMP_IF = 0x027,
+    NOT_EQUAL = 0x28,
 
-  AND,            // 19
-  OR,             // 1A
-  JMP,            // 1B
-  JF,             // 1C
-  CALL,           // 1D
-  RET,            // 1E
-  RET_VOID,       // 1F
-  HALT,           // 20
-  UNSET,          // 21
-  SLICE,          // 22
-  INDEX,          // 23
-  SET_INDEX,      // 24
-  TAIL,           // 25
-  YIELD,          // 26
-  NOT             // 27
-  NO_OP = 0x3C,
+
+  FUNCTION_REGISTER = 0x0F,
+    CALL = 0x029,
+    TAIL = 0x030,
+    RETURN = 0x031,
+    RETURN_VOID = 0x032,
+
+  HALT = 0x033,
+  SLICE = 0x034,
+  SET_INDEX = 0x035,
+  YIELD = 0x037,
+  NO_OP = 0x038,
+  POP = 0x039,
+  INDEX = 0x040,
 };
