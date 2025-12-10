fe086da93b5c39c7b28134b312ab6c8e13970b56
🍙 𝑹𝒖𝒓𝒊 🎋 <134459455+TokynBlast@users.noreply.github.com>
2025-12-10 11:45:32 -0800

    Added more compatible types

--- include/value.hpp
+++ include/value.hpp
@@ -2,9 +2,8 @@
 #include <variant>
 #include <vector>
 #include <sstream>
-#include "err.hpp"
+#include <stdint.h>
 #include "types.hpp"
-#include "pos.hpp"
 #include "sso.hpp"
 
 using namespace lang;
@@ -17,46 +16,64 @@ struct Value {
   // Accept a Source instead of a temporary string to keep a stable pointer.
   explicit Value(const Source& source) {
     P.src = source.name.c_str();  // Direct assignment to CString
-    P.line = 1;
-    P.col = 1;
   }
 
   Type t = Type::Null;
   std::variant<
-    std::monostate,          // Null
-    bool,                    // Bool
-    std::vector<Value>,      // List
-    long long,               // Int
-    CString,                 // Str
-    double                   // Float
+    CString,             // Str
+    int,                 // Int
+    bool,                // Bool
+    std::vector<Value>,  // List
+    double,              // Float
+    std::monostate,      // Null
+    int8_t,              // 8-bit int
+    int16_t,             // 16-bit int
+    int32_t,             // 32-bit int
+    int64_t,             // 64-bit int
+    __int128,            // GNU 128-bit int
+    uint8_t,             // unsigned 8-bit int
+    uint16_t,            // unsigned 16-bit int
+    uint32_t,            // unsigned 32-bit int
+    uint64_t,            // unsigned　64-bit int
+    unsigned __int128    // GNU unsigned 128-bit int
   > v;
 
   Value(Type type) : t(type) {}
   Value(Type type, bool b) : t(type), v(b) {}
-  Value(Type type, long long i) : t(type), v(i) {}
+  Value(Type type, int i) : t(type), v(i) {}
   Value(Type type, double f) : t(type), v(f) {}
   Value(Type type, const char* s) : t(type), v(CString(s)) {}
   Value(Type type, CString&& s) : t(type), v(std::move(s)) {}
   Value(Type type, const std::vector<Value>& list) : t(type), v(list) {}
-  Value(Type type, const Pos& pos) : t(type) { P = pos; }
+  Value(Type type, const int8_t i) : t(type), v(i) {}
+  Value(Type type, const int16_t i) : t(type), v(i) {}
+  Value(Type type, const int32_t i) : t(type), v(i) {}
+  Value(Type type, const int64_t i) : t(type), v(i) {}
+  Value(Type type, const __int128 i) : t(type), v(i) {}
+  Value(Type type, const uint8_t ui) : t(type), v(ui) {}
+  Value(Type type, const uint16_t ui) : t(type), v(ui) {}
+  Value(Type type, const uint32_t ui) : t(type), v(ui) {}
+  Value(Type type, const uint64_t ui) : t(type), v(ui) {}
+  Value(Type type, const unsigned __int128 ui) : t(type), v(ui) {}
 
   static Value N() { return Value(Type::Null); }
   static Value B(bool b) { return Value(Type::Bool, b); }
   static Value L(const std::vector<Value>& l) { return Value(Type::List, l); }
-  static Value I(long long i) { return Value(Type::Int, i); }
-  // FIXME: String should not copy
+  static Value I(int i) { return Value(Type::Int, i); }
+  // FIXME: String should not copy, too much possible overhead
   static Value S(const char* s) { return Value(Type::Str, s); }
   static Value S(CString&& s) { return Value(Type::Str, std::move(s)); }
   static Value F(double f) { return Value(Type::Float, f); }
-
-private:
-  Loc locate(std::size_t extra = 0) const {
-    Loc loc;
-    loc.line = static_cast<int>(P.line);
-    loc.col = static_cast<int>(P.col + extra);
-    loc.src = P.src;
-    return loc;
-  }
+  static Value I8(int8_t i) {return Value(Type::i8, i); }
+  static Value I16(int16_t i) {return Value(Type::i16, i); }
+  static Value I32(int32_t i) {return Value(Type::i32, i); }
+  static Value I64(int64_t i) {return Value(Type::i64, i); }
+  static Value I128(__int128 i) {return Value(Type::i128, i); }
+  static Value UI8(uint8_t ui) {return Value(Type::ui8, ui); }
+  static Value UI16(uint16_t ui) {return Value(Type::ui16, ui); }
+  static Value UI32(uint32_t ui) {return Value(Type::ui32, ui); }
+  static Value UI64(uint64_t ui) {return Value(Type::ui64, ui); }
+  static Value UI128(unsigned __int128 i) {return Value(Type::ui128, ui); }
 
 public:
   long long AsInt(auto loc) const {
