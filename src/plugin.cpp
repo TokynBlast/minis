f3bd20b237d38a526c9f7c9db793aabe4cf58f39
🍙 𝑹𝒖𝒓𝒊 🎋 <134459455+TokynBlast@users.noreply.github.com>
2025-12-11 14:18:41 -0800

    Added variables

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -1,97 +1,100 @@
+#pragma once
 #include <dlfcn.h>
 #include <unordered_map>
-#include <iostream>
-#include "../include/plugin.hpp"
+#include <vector>
+#include "value.hpp"
+#include "sso.hpp"
 
 namespace lang {
-
-// Global storage for loaded plugins
-struct LoadedPlugin {
+  using PluginFn = Value (*)(const std::vector<Value>&);
+  using PluginVar = const Value*;
+  
+  struct PluginFunctionEntry {
+    const char* name;
+    PluginFn function;
+    PluginVar variable;
+  };
+  
+  struct PluginInterface {
+    const char* name;
+    const char* version;
+    bool (*init)();
+    const PluginFunctionEntry* (*get_functions)();
+    void (*cleanup)();
+  };
+  
+  struct LoadedPlugin {
     void* handle;
-    PluginInterface interface;
-    std::unordered_map<CString, PluginFn> functions;
-};
-
-static std::unordered_map<CString, LoadedPlugin> loaded_plugins;
-static std::unordered_map<CString, PluginFn> plugin_functions;
-
-bool PluginManager::load_plugin(const char* plugin_name, const char* library_path) {
-    // Check if already loaded
-    if (loaded_plugins.find(plugin_name) != loaded_plugins.end()) {
-        return true; // Already loaded
-    }
-    
-    // Load shared library
-    void* handle = dlopen(library_path, RTLD_LAZY);
-    if (!handle) {
-        std::cerr << "Failed to load plugin " << plugin_name << ": " << dlerror() << std::endl;
-        return false;
-    }
-    
-    // Get plugin interface
-    typedef PluginInterface* (*GetPluginFunc)();
-    GetPluginFunc get_plugin = (GetPluginFunc)dlsym(handle, "get_plugin_interface");
-    if (!get_plugin) {
-        std::cerr << "Plugin " << plugin_name << " missing get_plugin_interface()" << std::endl;
+    PluginInterface iface;
+  };
+  
+  static std::unordered_map<CString, LoadedPlugin> loaded;
+  static std::unordered_map<CString, PluginFn> funcs;
+  static std::unordered_map<CString, PluginVar> vars;
+  
+  class PluginManager {
+  public:
+    static bool load_plugin(const char* plugin_name, const char* library_path) {
+      if (loaded.find(plugin_name) != loaded.end()) return true;
+  
+      void* handle = dlopen(library_path, RTLD_LAZY);
+      if (!handle) return false;
+  
+      using GetIface = PluginInterface* (*)();
+      GetIface get = (GetIface)dlsym(handle, "get_plugin_interface");
+      if (!get) { dlclose(handle); return false; }
+  
+      PluginInterface* iface = get();
+      if (!iface) { dlclose(handle); return false; }
+  
+      if (iface->init && !iface->init()) {
         dlclose(handle);
         return false;
-    }
-    
-    PluginInterface* iface = get_plugin();
-    if (!iface) {
-        std::cerr << "Plugin " << plugin_name << " returned NULL interface" << std::endl;
-        dlclose(handle);
-        return false;
-    }
-    
-    // Initialize plugin
-    if (iface->init && !iface->init()) {
-        std::cerr << "Plugin " << plugin_name << " initialization failed" << std::endl;
-        dlclose(handle);
-        return false;
-    }
-    
-    // Register functions
-    LoadedPlugin plugin;
-    plugin.handle = handle;
-    plugin.interface = *iface;
-    
-    if (iface->get_functions) {
-        const PluginFunctionEntry* funcs = iface->get_functions();
-        for (int i = 0; funcs[i].name != nullptr; ++i) {
-            CString fname = CString(plugin_name) + "_" + funcs[i].name + "_";
-            plugin.functions[fname] = funcs[i].function;
-            plugin_functions[fname] = funcs[i].function;
-            std::cout << "  Registered plugin function: " << fname.c_str() << std::endl;
+      }
+  
+      LoadedPlugin p;
+      p.handle = handle;
+      p.iface = *iface;
+  
+      if (iface->get_functions) {
+        const PluginFunctionEntry* f = iface->get_functions();
+        for (; f->name; ++f) {
+          CString full = CString(plugin_name) + "_" + f->name;
+          if (f->function) funcs[full] = f->function;
+          if (f->variable) vars[full] = f->variable;
         }
+      }
+  
+      loaded[plugin_name] = p;
+      return true;
     }
-    
-    loaded_plugins[plugin_name] = std::move(plugin);
-    std::cout << "✓ Loaded plugin: " << plugin_name << " v" << iface->version << std::endl;
-    return true;
-}
-
-PluginFn PluginManager::get_function(const char* name) {
-    auto it = plugin_functions.find(name);
-    if (it != plugin_functions.end()) {
-        return it->second;
+  
+    static PluginFn get_function(const char* name) {
+      auto it = funcs.find(name);
+      return (it == funcs.end()) ? nullptr : it->second;
     }
-    return nullptr;
-}
-
-bool PluginManager::has_function(const char* name) {
-    return plugin_functions.find(name) != plugin_functions.end();
-}
-
-void PluginManager::cleanup() {
-    for (auto& pair : loaded_plugins) {
-        if (pair.second.interface.cleanup) {
-            pair.second.interface.cleanup();
-        }
-        dlclose(pair.second.handle);
+  
+    static PluginVar get_variable(const char* name) {
+      auto it = vars.find(name);
+      return (it == vars.end()) ? nullptr : it->second;
     }
-    loaded_plugins.clear();
-    plugin_functions.clear();
+  
+    static bool has_function(const char* name) {
+      return funcs.find(name) != funcs.end();
+    }
+  
+    static bool has_variable(const char* name) {
+      return vars.find(name) != vars.end();
+    }
+  
+    static void cleanup() {
+      for (auto& p : loaded) {
+        if (p.second.iface.cleanup) p.second.iface.cleanup();
+        dlclose(p.second.handle);
+      }
+      loaded.clear();
+      funcs.clear();
+      vars.clear();
+    }
+  };
 }
-
-} // namespace lang
