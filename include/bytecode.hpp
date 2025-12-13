8a84076e69356da5a46e6cbb45496971c06cd161
🍙 𝑹𝒖𝒓𝒊 🎋 <134459455+TokynBlast@users.noreply.github.com>
2025-12-12 16:40:34 -0800

    Removed consexpr from enums

--- include/bytecode.hpp
+++ include/bytecode.hpp
@@ -1,7 +1,7 @@
 #pragma once
 #include <cstdint>
 
-constexpr enum class Register : std::uint8_t {
+enum class Register : std::uint8_t {
   IMPORT = 0x0A,
   VARIABLE = 0x0B,
   LOGIC = 0x0C,
@@ -9,13 +9,13 @@ constexpr enum class Register : std::uint8_t {
   GENERAL = 0x0E,
 }
 
-constexpr enum class Import : std::uint8_t {
+enum class Import : std::uint8_t {
   FUNC  = 0x01,
   LOAD,
   STORE
 }
 
-constexpr enum class Variable : std::uint8_t {
+enum class Variable : std::uint8_t {
   GET = 0x01,
   SET,
   DECLARE,
@@ -23,7 +23,7 @@ constexpr enum class Variable : std::uint8_t {
   PUSH
 }
 
-constexpr enum class Math : std::uint8_t {
+enum class Math : std::uint8_t {
   ADD = 0x01,
   SUBTRACT,
   MULTIPLY,
@@ -31,7 +31,7 @@ constexpr enum class Math : std::uint8_t {
   NEGATE
 }
 
-constexpr enum class Logic : std::uint8_t
+enum class Logic : std::uint8_t
   EQUAL = 0x01,
   NOT_EQUAL,
   LESS_THAN_OR_EQUAL,
@@ -42,14 +42,13 @@ constexpr enum class Logic : std::uint8_t
   JUMP_IF
 }
 
-constexpr enum class Func : std::uint8_t
+enum class Func : std::uint8_t
   CALL = 0x01,
   TAIL,
   RETURN,
   RETURN_VOID,
 }
-
-constexpr enum class General : std::uint8_t
+enum class General : std::uint8_t
   HALT = 0x01,
   SLICE,
   SET_INDEX,
