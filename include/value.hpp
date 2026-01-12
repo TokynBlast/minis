6d58bb9b0fff0cca5b7eeb803d249197ea6cfe59
Ruri/Mei De Jishi <tokynblast@gmail.com>
2026-01-12 09:53:29 -0800

    Change CString to std::string

--- include/value.hpp
+++ include/value.hpp
@@ -4,9 +4,9 @@
 #include <vector>
 #include <map>
 #include <sstream>
+#include <string>
 #include <stdint.h>
 #include "types.hpp"
-#include "sso.hpp"
 
 using namespace minis;
 
@@ -16,18 +16,18 @@ struct Value
 
   Type t = Type::Null;
   std::variant<
-      CString,                // Str
       int,                    // Int
       bool,                   // Bool
+      std::string,            // String
       std::vector<Value>,     // List
       std::map<Value, Value>, // dictionary
       std::map<int, int>,     // range
       double,                 // float
       std::monostate,         // null
-      int8,                 // 8-bit int
-      int16,                // 16-bit int
-      int32,                // 32-bit int
-      int64,                // 64-bit int
+      int8,                   // 8-bit int
+      int16,                  // 16-bit int
+      int32,                  // 32-bit int
+      int64,                  // 64-bit int
       uint8,                  // unsigned 8-bit int
       uint16,                 // unsigned 16-bit int
       uint32,                 // unsigned 32-bit
@@ -39,11 +39,15 @@ struct Value
   Value(Type type, bool b) : t(type), v(b) {}
   Value(Type type, int i) : t(type), v(i) {}
   Value(Type type, double f) : t(type), v(f) {}
-  Value(Type type, const char *s) : t(type), v(CString(s)) {}
-  Value(Type type, CString &&s) : t(type), v(std::move(s)) {}
+  Value(Type type, const char *s) : t(type), v(std::string(s)) {}
+  Value(Type type, std::string &&s) : t(type), v(std::move(s)) {}
   Value(Type type, const std::vector<Value> &list) : t(type), v(list) {}
   Value(Type type, const std::map<Value, Value> &dict) : t(type), v(dict) {}
   Value(Type type, const std::map<int, int> &range) : t(type), v(range) {}
+  Value(Type type, int8 i) : t(type), v(i) {}
+  Value(Type type, int16 i) : t(type), v(i) {}
+  Value(Type type, int32 i) : t(type), v(i) {}
+  Value(Type type, int64 i) : t(type), v(i) {}
   Value(Type type, uint8 i) : t(type), v(i) {}
   Value(Type type, uint16 i) : t(type), v(i) {}
   Value(Type type, uint32 i) : t(type), v(i) {}
@@ -54,7 +58,7 @@ struct Value
   static Value L(const std::vector<Value> &l) { return Value(Type::List, l); }
   static Value I(int i) { return Value(Type::Int, i); }
   static Value S(const char *s) { return Value(Type::Str, s); }
-  static Value S(CString &&s) { return Value(Type::Str, std::move(s)); }
+  static Value S(std::string &&s) { return Value(Type::Str, std::move(s)); }
   static Value F(double f) { return Value(Type::Float, f); }
   static Value I8(int8 i) { return Value(Type::i8, i); }
   static Value I16(int16 i) { return Value(Type::i16, i); }
@@ -96,7 +100,7 @@ struct Value
       return (int)std::get<uint64>(v);
     case Type::Str:
     {
-      const CString &s = std::get<CString>(v);
+      const std::string &s = std::get<std::string>(v);
       return std::stoll(s.c_str());
     }
     default:
@@ -134,7 +138,7 @@ struct Value
       return (double)std::get<uint64>(v);
     case Type::Str:
     {
-      const CString &s = std::get<CString>(v);
+      const std::string &s = std::get<std::string>(v);
       return std::stod(s.c_str());
     }
     default:
@@ -170,7 +174,7 @@ struct Value
       return std::get<uint64>(v) != 0;
     case Type::Str:
     {
-      const CString &s = std::get<CString>(v);
+      const std::string &s = std::get<std::string>(v);
       if (s == "true")
         return true;
       if (s == "false")
@@ -191,85 +195,85 @@ struct Value
     switch (t)
     {
     case Type::Str:
-      return std::get<CString>(v).c_str();
+      return std::get<std::string>(v).c_str();
     case Type::Int:
     {
-      static CString int_str;
+      static std::string int_str;
       std::ostringstream os;
       os << std::get<int>(v);
-      int_str = os.str().c_str();
+      int_str = os.str();
       return int_str.c_str();
     }
     case Type::i8:
     {
-      static CString i8_str;
+      static std::string i8_str;
       std::ostringstream os;
       os << (int)std::get<int8>(v);
-      i8_str = os.str().c_str();
+      i8_str = os.str();
       return i8_str.c_str();
     }
     case Type::i16:
     {
-      static CString i16_str;
+      static std::string i16_str;
       std::ostringstream os;
       os << std::get<int16>(v);
-      i16_str = os.str().c_str();
+      i16_str = os.str();
       return i16_str.c_str();
     }
     case Type::i32:
     {
-      static CString i32_str;
+      static std::string i32_str;
       std::ostringstream os;
       os << std::get<int32>(v);
-      i32_str = os.str().c_str();
+      i32_str = os.str();
       return i32_str.c_str();
     }
     case Type::i64:
     {
-      static CString i64_str;
+      static std::string i64_str;
       std::ostringstream os;
       os << std::get<int64>(v);
-      i64_str = os.str().c_str();
+      i64_str = os.str();
       return i64_str.c_str();
     }
     case Type::ui8:
     {
-      static CString ui8_str;
+      static std::string ui8_str;
       std::ostringstream os;
       os << (unsigned)std::get<uint8>(v);
-      ui8_str = os.str().c_str();
+      ui8_str = os.str();
       return ui8_str.c_str();
     }
     case Type::ui16:
     {
-      static CString ui16_str;
+      static std::string ui16_str;
       std::ostringstream os;
       os << std::get<uint16>(v);
-      ui16_str = os.str().c_str();
+      ui16_str = os.str();
       return ui16_str.c_str();
     }
     case Type::ui32:
     {
-      static CString ui32_str;
+      static std::string ui32_str;
       std::ostringstream os;
       os << std::get<uint32>(v);
-      ui32_str = os.str().c_str();
+      ui32_str = os.str();
       return ui32_str.c_str();
     }
     case Type::ui64:
     {
-      static CString ui64_str;
+      static std::string ui64_str;
       std::ostringstream os;
       os << std::get<uint64>(v);
-      ui64_str = os.str().c_str();
+      ui64_str = os.str();
       return ui64_str.c_str();
     }
     case Type::Float:
     {
-      static CString float_str;
+      static std::string float_str;
       std::ostringstream os;
       os << std::get<double>(v);
-      float_str = os.str().c_str();
+      float_str = os.str();
       return float_str.c_str();
     }
     case Type::Bool:
@@ -278,7 +282,7 @@ struct Value
       return "null";
     case Type::List:
     {
-      static CString list_str;
+      static std::string list_str;
       const auto &xs = std::get<std::vector<Value>>(v);
       std::ostringstream os;
       os << "[";
@@ -289,7 +293,7 @@ struct Value
         os << xs[i].AsStr();
       }
       os << "]";
-      list_str = os.str().c_str();
+      list_str = os.str();
       return list_str.c_str();
     }
     default:
@@ -331,7 +335,7 @@ struct Value
     case Type::ui64:
       return std::get<uint64>(v) == std::get<uint64>(other.v);
     case Type::Str:
-      return std::get<CString>(v) == std::get<CString>(other.v);
+      return std::get<std::string>(v) == std::get<std::string>(other.v);
     case Type::List:
       return std::get<std::vector<Value>>(v) ==
              std::get<std::vector<Value>>(other.v);
