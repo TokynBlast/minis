0160242ad1eea8c692806263a79e8d99ea6928b5
Ruri/Mei De Jishi <tokynblast@gmail.com>
2026-01-12 13:35:08 -0800

    Add use of std::string over const char

--- include/buffer.hpp
+++ include/buffer.hpp
@@ -1,31 +1,24 @@
 #pragma once
-#include <cstdio>
-#include <cstring>
+#include <string>
+#include <iostream>
 
 #define MAX_CHARS 4096
 
 namespace minis {
   struct OutBuffer {
     static constexpr size_t BUF_SIZE = MAX_CHARS;
-    char buf[BUF_SIZE];
-    size_t pos = 0;
+    std::string buf;
 
     void write(const char* s) {
-      size_t len = std::strlen(s);
-      if (pos + len >= BUF_SIZE) flush();
-      if (len >= BUF_SIZE) {
-        fwrite(s, 1, len, stdout);
-      } else {
-        std::memcpy(buf + pos, s, len);
-        pos += len;
-      }
+      buf += s;
+      if (buf.size() >= BUF_SIZE) flush();
     }
 
     void flush() {
-      if (pos > 0) {
-        fwrite(buf, 1, pos, stdout);
-        fflush(stdout);
-        pos = 0;
+      if (!buf.empty()) {
+        std::cout << buf;
+        std::cout.flush();
+        buf.clear();
       }
     }
 
--- include/err_vm.hpp
+++ include/err_vm.hpp
@@ -1,15 +1,14 @@
 #pragma once
 #include <cstdlib>
 // FiXME: Should use Minis buffer instead
-#include <iostream>
+#include "buffer.hpp"
+#include <string>
 
 namespace minis {
   // VM error: always fatal, no location
-  inline void fatal(const char* msg) {
-    // FIXME: Should use Minis buffer instead
-    std::cerr << "[FATAL ERROR]: " << msg << '\n';
+  inline void fatal(std::string msg) {
+    screen.write("FATAL ERROR: " + msg + "\n");
     std::exit(1);
   }
-
   #define VM_ERR(MSG) ::minis::vm_fatal(MSG)
 }
