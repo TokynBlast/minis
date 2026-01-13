21dd6a741cbbd49239777bb5a1c0645313d297bd
Ruri/Mei De Jishi <tokynblast@gmail.com>
2026-01-13 14:35:45 -0800

    Convert to .h
    This allows us to use C23 things

--- include/macros.h
+++ include/macros.h
@@ -16,6 +16,8 @@
 #define int32 int32_t
 #define int64 int64_t
 
+#define int DONT_USE_INT___USE_FIXED_SIZES
+
 #if !defined(_WIN32) && !defined(__linux__) && !defined(__APPLE__)
   #error "Use Windows, Linux, or Apple."
 #endif
@@ -28,6 +30,9 @@
   #define nouse __attribute__((unused))
   #define uint128 unsigned __int128
   #define int128 __int128
+  // Add the folliowing with boost::multiprecision
+  //#define uint256 unsigned _BitInt(256)
+  //#define int256 _BitInt(256)
 #else
   #error "Use g++ or Clang to compile."
 #endif
