5a805ad3892d5536f8952b0c5982e6fe8e0e2c64
Ruri/Mei De Jishi <tokynblast@gmail.com>
2026-01-14 08:17:48 -0800

    Move computer.cpp

--- plugins/computer/computer.cpp
+++ plugins/computer/computer.cpp
@@ -6,50 +6,45 @@ using namespace minis;
 
 static Value os(const std::vector<Value>& args) {
   #if defined(_WIN32) || defined(_WIN64)
-    return Value::S("win");
+    return Value::Str("win");
   #elif defined(__linux__) || defined(__unix__)
-    return Value::S("linux");
+    return Value::Str("linux");
   #elif defined(__FreeBSD__) || defined(__FreeBSD_kernel__)
-    return Value::S("freebsd");
+    return Value::Str("freebsd");
   #elif defined(__APPLE__) || defined(__MACH__)
-    return Value::S("apple");
+    return Value::Str("apple");
   #elif defined(sun) || defined(__sun__)
-    return Value::S("solaris");
+    return Value::Str("solaris");
   #elif defined(_AIX)
-    return Value::S("aix");
+    return Value::Str("aix");
   #elif defined(__hpux)
-    return Value::S("hp-ux");
+    return Value::Str("hp-ux");
   #else
-    return Value::S("unknown");
+    return Value::Str("unknown");
   #endif
 }
 
-static Value int_size() {
-  return Value::I(sizeof(int));
-}
-
-static Value cpu() {
+static Value cpu(std::vector<Value>& args) {
   #if defined(__i386__) || defined(i386)
-    return Value::S("x86");
+    return Value::Str("x86");
   #elif defined(__x86_64__) || defined(M_X64)
-    return Value::S("x86-64");
+    return Value::Str("x86-64");
   #elif defined(__ARM__)
-    return Value::S("arm");
+    return Value::Str("arm");
   #elif defined(__aarch_64__)
-    return Value::S("arm64");
+    return Value::Str("arm64");
   #elif defined(__ppc__) || defined(__powerpc__)
-    return Value::S("powerpc");
+    return Value::Str("powerpc");
   #elif defined(__sparc__)
-    return Value::S("sparc");
+    return Value::Str("sparc");
   #else
-    return Value::S("unknown");
+    return Value::Str("unknown");
   #endif
 }
 
 static const PluginFunctionEntry plugin_functions[] = {
   {"os", os},
   {"cpu", cpu},
-  {"sint", int_size},
   {nullptr, nullptr}
 };
 
@@ -58,7 +53,7 @@ static void computer_cleanup() {}
 
 static PluginInterface random_interface = {
     "computer",
-    "0.0.2",
+    "0.0.3",
     computer_init,
     []() -> const PluginFunctionEntry* { return plugin_functions; },
     computer_cleanup
