7bd05bfbc85017eb2eb284cd63eb913fc663004b
Ruri/Mei De Jishi <tokynblast@gmail.com>
2026-01-21 08:27:55 -0800

    Add preprocessor

--- compiling/compiler.asm
+++ compiling/compiler.asm
@@ -1,9 +1,82 @@
 ; First iteration of Minis
-.main:
-  push "compiler.asm"
-  push "r"
-  call "open" 2
-  set file
+.main
+  push "compiler.asm"    ; file name
+  call "read" 1          ; read file
+  set contents           ; set the contents of file
 
-  get file
-  call "read" 1
+.preprocess              ; remove comments (like these), then empty lines
+  get contents           ; get contents variable
+  push "\n"              ; split by \n
+  call "split" 2         ; call split
+  set lines              ; set the split list
+
+  set list output []     ; all the lines :D
+
+  set str new_line ""
+
+  set ui64 pos 0         ; set pointer position
+
+  while_loop:
+    get pos              ; get loop starter
+    get lines            ; get lines
+    call "len" 1         ; get length of lines
+    lt                   ; check if i < len of lines
+    jmpifn pre_proc_end  ; go to pre_proc_end if true
+
+
+    ; Loop body - process line
+    get lines            ; get all lines
+    get pos              ; get pos in lines
+    call "index" 2       ; get line at pos
+    push ""              ; set what to split by
+    call "split" 2       ; split every char
+    set curr_line        ; set list to curr_line
+
+    set i32 line_pos 0
+
+    while_in_comment:
+      ; Increment line, then jump to while_in_comment :)
+
+      ; check for end of line :3
+      get line_pos
+      get curr_line
+      call "len" 1
+      lt
+      jmpif increment_line
+
+      ; Set the current character
+      get curr_line
+      get line_pos
+      call "index" 2
+      set curr_char
+
+      get curr_char
+      push "\;"
+      eq
+      jmpif increment_line
+
+    increment_line:
+      set line_pos 0
+      get curr_line
+      push 1
+      add
+
+      get pos
+
+      jmp while_in_comment
+
+  pre_proc_end:
+    push "\n"
+    get output
+    call "print" 2
+    jmp prog_end
+
+prog_end:
+  ; Free up al variables :)
+  ;unset contents
+  ;unset lines
+  ;unset pos
+  ;unset curr_line
+  ;unset line_pos
+  ;unset output
+  halt
