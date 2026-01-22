bddd2a24a2cf75405c6b5521b3b3ed8193e821ae
Ruri/Mei De Jishi <tokynblast@gmail.com>
2026-01-21 16:09:03 -0800

    Update instruction order

--- compiling/compiler.asm
+++ compiling/compiler.asm
@@ -1,8 +1,11 @@
 ; First iteration of Minis
 .main
+  ; str contents = read("compiler.asm")
   push "compiler.asm"    ; file name
   call "read" 1          ; read file
   set contents           ; set the contents of file
+
+  jmp preprocess
 
 .preprocess              ; remove comments (like these), then empty lines
   get contents           ; get contents variable
@@ -16,42 +19,42 @@
 
   set ui64 pos 0         ; set pointer position
 
+  jmp while_loop
+
   while_loop:
-    get pos              ; get loop starter
     get lines            ; get lines
     call "len" 1         ; get length of lines
+    get pos              ; get loop starter
     lt                   ; check if i < len of lines
-    jmpifn pre_proc_end  ; go to pre_proc_end if true
+    jmpif pre_proc_end   ; go to pre_proc_end if true
 
 
     ; Loop body - process line
     get lines            ; get all lines
     get pos              ; get pos in lines
     call "index" 2       ; get line at pos
-    push ""              ; set what to split by
-    call "split" 2       ; split every char
-    set curr_line        ; set list to curr_line
+    set curr_line        ; set curr_line to the current line
 
-    set i32 line_pos 0
+    set i64 line_pos 0
 
     while_in_comment:
       ; Increment line, then jump to while_in_comment :)
 
       ; check for end of line :3
-      get line_pos
       get curr_line
       call "len" 1
+      get line_pos
       lt
       jmpif increment_line
 
       ; Set the current character
       get curr_line
       get line_pos
-      call "index" 2
+      index
       set curr_char
 
       get curr_char
-      push ";"
+      push str ";" ; we should skip if we're in double quotes :)
       eq
       jmpif increment_line
 
@@ -68,6 +71,7 @@
 
     increment_line:
       set line_pos 0
+
       get curr_line
       push 1
       add
@@ -76,7 +80,7 @@
       get output
       get new_line
       add
-      set contents
+      set output
 
       push ""
       set new_line
