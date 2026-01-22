0ac50ee316f92d0ae4bdf2ed831c3594e4f0e394
🍙 𝑹𝒖𝒓𝒊 🎋 <tokynblast@gmail.com>
2026-01-22 10:14:12 -0800

    set ouput [] -> push [] set output

--- compiling/compiler.asm
+++ compiling/compiler.asm
@@ -1,11 +1,10 @@
-; First iteration of Minis
 .main
-  ; str contents = read("compiler.asm")
-  push "compiler.asm"    ; file name
-  call "read" 1          ; read file
-  set contents           ; set the contents of file
+  push "compiler.asm"
+  call "read" 1
+  set contents
 
   jmp preprocess
+
 
 .preprocess
   get contents
@@ -13,48 +12,82 @@
   call "split" 2
   set lines
 
-  set list output []
+  push []
+  declare output
   set ui64 pos 0
+
 
 while_loop:
   get lines
   call "len" 1
+  set lines_len
+
   get pos
+  get lines_len
   lt
+  set cond_while
+
+  get cond_while
   jmpifn pre_proc_end
+
 
   get lines
   get pos
-  call "index" 2
+  index
   set curr_line
 
   set str new_line ""
   set i64 line_pos 0
   set bool in_quotes false
 
+
 char_loop:
   get curr_line
   call "len" 1
+  set curr_line_len
+
   get line_pos
+  get curr_line_len
   lt
+  set cond_char
+
+  get cond_char
   jmpifn end_line
+
 
   get curr_line
   get line_pos
   index
   set curr_char
 
+
+  ; ────────────── QUOTE TOGGLE ──────────────
   get curr_char
   push "\""
   eq
   jmpif toggle_quotes
 
+
+  ; ────────────── COMMENT CHECK ──────────────
   get curr_char
   push ";"
   eq
-  get in_quotes
-  jmpifn end_line
+  set is_semicolon
 
+  get in_quotes
+  not
+  set not_in_quotes
+
+  get is_semicolon
+  get not_in_quotes
+  and
+  set is_comment_cut
+
+  get is_comment_cut
+  jmpif end_line
+
+
+  ; ────────────── NORMAL CHAR ──────────────
   get new_line
   get curr_char
   add
@@ -64,56 +97,64 @@ char_loop:
   push 1
   add
   set line_pos
+
   jmp char_loop
+
 
 toggle_quotes:
   get in_quotes
-  push 0
-  eq
-  jmpif set_quotes_true
-  push 0
-  set in_quotes
-  jmp after_toggle
-
-set_quotes_true:
-  push 1
+  not
   set in_quotes
 
-after_toggle:
   get new_line
   get curr_char
   add
   set new_line
+
   get line_pos
   push 1
   add
   set line_pos
+
   jmp char_loop
+
 
 end_line:
   get new_line
   call "len" 1
+  set new_line_len
+
+  get new_line_len
   push 0
   eq
+  set is_empty_line
+
+  get is_empty_line
   jmpif next_line
 
   get output
   get new_line
-  call "append" 2
+  add
   set output
+
 
 next_line:
   get pos
   push 1
   add
   set pos
+
   jmp while_loop
 
+
 pre_proc_end:
-  push "\n"
   get output
-  call "print" 2
+  call "print" 1
+  push "\n"
+  call "print" 1
+
   jmp prog_end
+
 
 .prog_end
   halt
