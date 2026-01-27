e0d703359652fe60e95caa6b7623135da727d657
🍙 𝑹𝒖𝒓𝒊 🎋 <tokynblast@gmail.com>
2026-01-27 08:42:23 -0800

    Add vector macro for AST

---
+++ compiling/vect.h
@@ -0,0 +1,9 @@
+#define VEC(T) struct { T *data; size_t len, cap; }
+
+#define push(v, x) do { \
+  if ((v)->len == (v)->cap) { \
+    (v)->cap = (v)->cap ? (v)->cap + 2 : 8; \
+    (v)->data = realloc((v->data), (v)->cap * sizeof(*(v)->data)); \
+  } \
+   (v)->data[(v)->len++] = (x); \
+} while (0) \
